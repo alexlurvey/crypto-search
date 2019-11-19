55013dff0cf3a34a3c197b2c240cf3ee732a9d58
alexlurvey <alex.lurvey@gmail.com>
2019-11-18 19:53:33 -0600

    update hdom-component language

--- src/hdom-components/root.js
+++ src/hdom-components/root.js
@@ -5,7 +5,7 @@ import { pageLoading } from './page-loading';
 export const root = (ctx, dataLoading, cryptocurrencies, search, orderByMarketCap) => {
     return ['section.section.app-wrapper',
             ['div.container',
-                ['h1.title', 'Search CryptoCurrencies'],
+                ['h1.title', 'Search Cryptocurrencies'],
                 filters(ctx, search, orderByMarketCap),
                 dataLoading
                     ? pageLoading(dataLoading)
